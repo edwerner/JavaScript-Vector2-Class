3e5416d2ff502d41e4dc3fef1cc39907382d4fbd
Ed Werner <moshmartyr@gmail.com>
2012-07-21 22:00:00 -0700

    custom event binding added

--- vector2d.js
+++ vector2d.js
@@ -1,3 +1,14 @@
+
+//
+// Vector2D has separate model which gets
+//	updated by accessors and mutators from
+//	Vehicle subclass' model
+//
+//
+//
+//
+//
+
 var Vector2D = function( )
 {
 	this._x = undefined;
@@ -51,21 +62,115 @@ var Vector2D = function( )
 
 var vehicle = Object.create( Vector2D );
 
+vehicle.values = {	
+
+	getWidth : function( )
+	{
+		return this._width;
+	},
+	
+	setWidth : function( width )
+	{
+		this._width = width;
+	},
+	
+	getHeight : function( )
+	{
+		return this._height;
+	},
+	
+	setHeight : function( height )
+	{
+		this._height = height;
+	},
+	
+	getX : function( )
+	{
+		return this._x;
+	},
+	
+	setX : function( x )
+	{
+		this._x = x;
+	},
+	
+	getY : function( )
+	{
+		return this._y;
+	},
+	
+	setY : function( y )
+	{
+		this._y = y;
+	}
+};
+
+// augment Function.prototype so all can reach it
+// to define custom events and binding
+Function.prototype.method = function( name, func )
+{
+	this.prototype[ name ] = func;
+	return this;
+}
+
+// access prototype.method to define new behavior
+Function.method( 'bind', function( object )
+{
+	// return a function that will call this
+	// function as though it is a method of
+	// that object
+	
+	var method = this;
+	var slice = Array.prototype.slice;
+	var args = slice.apply( arguments, [ 1 ] );
+	
+	return function( )
+	{
+		return method.apply( object, args.concat( slice.apply( arguments, [ 0 ] ) ) );		
+	};
+} );
+
 vehicle.setup = function( )
 {
 	console.log( "Vehicle.initialize( )" );
 	
 	// get <canvas> element from the DOM
 	this.canvas = document.getElementById( 'vector-field' );
+	
+	// create and store reference to button
+	// then create event handler
+	var button = document.getElementById( 'spawn' );
+	button.addEventListener( "click", vehicle.onButtonClick );
 }
 
-vehicle.values = {
-	width : 100,
-	height : 100,
-	x : 100,
-	y : 100
-};
+// handle button click event
+vehicle.onButtonClick = function( )
+{
+	return button;
+}.bind( { button : vehicle.onButtonClickHandler } );
+	
+vehicle.onButtonClickHandler = function( )
+{
+	console.log( 'new vehicle added' );
+}
 
+// add new vehicle and set attributes
+vehicle.addNewVehicle = function( width, height, x, y )
+{
+	vehicle.values.setWidth( width );
+	vehicle.values.setHeight( height );
+	vehicle.values.setX( x );
+	vehicle.values.setY( y );
+}
+
+// log vector object params
+vehicle.log = function( )
+{
+	console.log( 'adding new vehicle' );
+	
+	/*console.log( 'this.object.getX( ) : ', this.object.getX( ) );
+	console.log( 'vehicle.values.getX( ) : ', vehicle.values.getX( ) );*/
+}
 console.log( vehicle.values );
 
 vehicle.createNewVector = function( )
@@ -73,38 +178,58 @@ vehicle.createNewVector = function( )
 	console.log( 'creating a new vector' );
 	
 	this.object = new Vector2D( );
-	this.object.setX( vehicle.values.width );
-	this.object.setY( vehicle.values.y );
-	this.object.setHeight( vehicle.values.height );
-	this.object.setWidth( vehicle.values.width );
+	
+	// vector instance uses getters and setters
+	// to update values from the vehicle class
+	this.object.setX( vehicle.values.getX( ) );
+	this.object.setY( vehicle.values.getY( ) );
+	this.object.setHeight( vehicle.values.getHeight( ) );
+	this.object.setWidth( vehicle.values.getWidth( ) );
 }
 
 vehicle.animate = function( )
 {
-	    // shim layer with setTimeout fallback
+	// shim layer with setTimeout fallback
 	window.requestAnimFrame = ( function( )
-	{
+	{		
   		return  window.requestAnimationFrame       || 
 		  window.webkitRequestAnimationFrame || 
 		  window.mozRequestAnimationFrame    || 
 		  window.oRequestAnimationFrame      || 
 		  window.msRequestAnimationFrame     || 
-		  function( vehicle.draw )
+		  function( draw )
 		  {
-			window.setTimeout( vehicle.draw, 1000 / 60 );
+			window.setTimeout( draw, 1000 / 60 );
 		  };
     } )( );
+    
+    // create infinite game loop
+    ( function loop( )
+    {
+      requestAnimFrame( loop );
+      vehicle.draw( );
+    } )( );
+    
+	for ( var key in this.values ) 
+	{
+	  if ( this.values.hasOwnProperty( key ) ) 
+	  {
+		// alert( key + " : " + this.values[ key ] );
+		vehicle.log( );
+	  }
+	}
 }
-
+    
 vehicle.draw = function( )
 {
 	// Make sure we don't execute when canvas isn't supported
 	if ( this.canvas.getContext )
 	{
+		// console.log( 'frame loop' );
 		// use getContext to use the canvas for drawing
 		var ctx = this.canvas.getContext( '2d' );
+		
 		// Draw shapes
-
 		var x = parseInt( vehicle.object.getX( ), 10 );
 		var y =  parseInt( vehicle.object.getY( ), 10 );
 		var width =  parseInt( vehicle.object.getWidth( ), 10 );
@@ -130,6 +255,7 @@ vehicle.draw = function( )
 		ctx.fill( );
 		ctx.stroke( );
 		ctx.restore( );
+		
 	} 
 	else 
 	{
@@ -140,16 +266,17 @@ vehicle.draw = function( )
 vehicle.initialize = function( )
 {
 	vehicle.setup( );
+	vehicle.addNewVehicle( 100, 100, 100, 100 );
 	vehicle.createNewVector( );
-	// vehicle.animate( );
+	vehicle.animate( );
 }
 
 
 // kick off the event chain
 if ( document.addEventListener )
-{  
-  document.addEventListener( "onload", vehicle.initialize( ) );
-} 
+{  	
+	document.addEventListener( "onload", vehicle.initialize( ) );
+}
 else if ( document.attachEvent )
 {  
   document.attachEvent( "onload", vehicle.initialize( ) );  
